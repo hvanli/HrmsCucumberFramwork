cb7f0193750343d0308143a1e394e60dc3b0bc19
hvanli <hvanli894@gmail.com>
2020-08-04 17:20:12 -0500

    Last Update on API

--- src/test/java/Practices/API_Recap.java
+++ src/test/java/Practices/API_Recap.java
@@ -2,4 +2,18 @@ package Practices;
 
 public class API_Recap {
 
+	/* How to automate API on Cucumber framework
+	 * 1. Create folder for .json files --> to save request bodies in json format
+	 * 2. Save json filepaths into API_Contants class
+	 * 3. Create a method to read json file inside CommonMethods
+	 * 4. Create ApiRunner class into runners package
+	 * 5. Create feature file for ApiRequest and write scenario
+	 * 6. Run ApiRunner while dryRun=true
+	 * 7. Include unimplemented steps into ApiStep class
+	 * 8. Write down your logic for the steps  
+	 * 
+	 * serialization and deserialization for API Testing
+
+
+*/
 }
--- src/test/java/com/hrms/testbase/BaseClass.java
+++ src/test/java/com/hrms/testbase/BaseClass.java
@@ -10,37 +10,37 @@ import com.hrms.utils.Constants;
 
 public class BaseClass{
 
-//	public static WebDriver driver;
-//
-//	
-//	/**
-//	 * Use this method in need of opening browser and target url
-//	 * @param browser The desired browser
-//	 * @param url	 The desired url
-//	 */	
-//	public static void setUp() {
-//		
-//		Properties configs=ConfigsReader.readProperties(Constants.CREDENTIALS_FILEPATH);
-//		
-//		switch(configs.getProperty("browser").toLowerCase()) {		
-//		case"chrome":
-//			System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH);
-//			driver=new ChromeDriver();
-//			break;
-//		case"firefox":
-//			System.setProperty("webdriver.gecko.driver", Constants.GECKO_DRIVER_PATH);
-//			driver=new FirefoxDriver();
-//			break;
-//		default:
-//			System.err.println("Browser is not supported");
-//		}		
-//		driver.manage().timeouts().pageLoadTimeout(Constants.PAGE_LOAD_TIME,  TimeUnit.SECONDS);
-//		driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_LOAD_TIME,  TimeUnit.SECONDS);
-//		driver.manage().window().maximize();		
-//		driver.get(configs.getProperty("url"));		
-//	}
-//	
-//	public static void tearDown() {
-//			driver.quit();		
-//	}
+ 	public static WebDriver driver;
+
+//	ChromeDriver implements WebDriver interface
+	/**
+	 * Use this method in need of opening browser and target url
+	 * @param browser The desired browser
+	 * @param url	 The desired url
+	 */	
+	public static void setUp() {
+		
+		Properties configs=ConfigsReader.readProperties(Constants.CREDENTIALS_FILEPATH);
+		
+		switch(configs.getProperty("browser").toLowerCase()) {		
+		case"chrome":
+			System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH);
+			driver=new ChromeDriver();
+			break;
+		case"firefox":
+			System.setProperty("webdriver.gecko.driver", Constants.GECKO_DRIVER_PATH);
+			driver=new FirefoxDriver();
+			break;
+		default:
+			System.err.println("Browser is not supported");
+		}		
+		driver.manage().timeouts().pageLoadTimeout(Constants.PAGE_LOAD_TIME,  TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_LOAD_TIME,  TimeUnit.SECONDS);
+		driver.manage().window().maximize();		
+		driver.get(configs.getProperty("url"));		
+	}
+	
+	public static void tearDown() {
+			driver.quit();		
+	}
 }
